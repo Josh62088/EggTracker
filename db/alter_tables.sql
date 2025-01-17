8fa32fa6024484be3dd37e1f349cb09eb2c913d0
RCMinute1 <josh62088@gmail.com>
2025-01-16 20:46:09 -0700

    Update index.html with new form and table structures, update related files

---
+++ db/alter_tables.sql
@@ -0,0 +1,1 @@
+ALTER TABLE egg_logs ADD COLUMN carton_assignment INTEGER;
---
+++ db/seed_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO egg_logs (date, eggs, usable_eggs, carton_id) VALUES
+('2023-01-01', 12, 12, 1),
+('2023-01-02', 10, 9, 2);
+
+INSERT INTO cartons (expiration_date, capacity, date_collected) VALUES
+('2023-01-15', 12, '2023-01-01'),
+('2023-01-16', 12, '2023-01-02');
