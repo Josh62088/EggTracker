1cc885ebae130256d7da3577045740a5c0f521a0
RCMinute1 <josh62088@gmail.com>
2025-01-16 22:24:10 -0700

    Update alter_tables.sql for carton_id as INTEGER

--- db/alter_tables.sql
+++ db/alter_tables.sql
@@ -1,1 +1,13 @@
 ALTER TABLE egg_logs ADD COLUMN carton_assignment INTEGER;
+
+CREATE TABLE egg_logs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    eggs INTEGER NOT NULL,
+    usable_eggs INTEGER,
+    carton_id INTEGER,
+);
+
+INSERT INTO egg_logs (id, date, eggs, usable_eggs, carton_id)
+SELECT id, date, eggs, usable_eggs, NULL FROM temp_egg_logs;
+DROP TABLE temp_egg_logs;
